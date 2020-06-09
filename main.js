47f9b9237aed6e526f6105e1bd234a9ac93a57fb
develo-pyo <jungpyo9@gmail.com>
2020-06-09 14:42:43 +0900

    add sampletxt

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+const url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(url == '/'){
-      url = '/index.html';
-    } else if(url == '/favicon.ico'){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    if(_url == '/'){
+      _url = '/index.html';
+    } else if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
+    //response.end(fs.readFileSync(__dirname + url));
  
 });
 app.listen(3000);
